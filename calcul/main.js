4d0a95d147762d15cdc19254fee924ce3881cc54
Cal <43823915+CalRL@users.noreply.github.com>
2022-04-07 14:04:34 +0200

    Calcul Azimuth/Altitude

--- calcul/main.js
+++ calcul/main.js
@@ -10,11 +10,17 @@ var height = 486
 var sunpos = SunCalc.getPosition(new Date, lat, lng)
 console.log(sunpos)
 
+var rndmAltitude = sunpos.altitude 
 var rndmAzimuth = sunpos.azimuth // *180/Math.PI;
-console.log(rndmAzimuth)
+console.log(rndmAzimuth, rndmAltitude)
+var aziRadToDeg = 1*rndmAzimuth*180/Math.pi
+var altRadToDeg = 1*rndmAltitude*180/Math.pi
+console.log(altRadToDeg, aziRadToDeg)
 
-var times = SunCalc.getTimes(new Date(), lat, lng);
+var times = SunCalc.getTimes(new Date(), lat, lng, height);
 console.log(times)
+//console.log(object.azimuth)
+//console.log(object.altitude)
 // url builder
 //URL (IA): https://www.suncalc.org/#/45.93668,6.13304,15/2015.05.11/13:15/1/2
 //var url = 'https://www.suncalc.org/#/45.93668,6.13304,15/'+ yyyy + '.' + mm + '.' + dd + '/' + hours + ':' + mins + '/1/2'
